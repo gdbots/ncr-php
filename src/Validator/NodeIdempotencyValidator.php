be5c5c1151693352fc4dd641e51291252fdda5f5
Greg Brown <greg@12dnetworks.com>
2018-12-04 09:15:28 -0800

    enhance idempotency validator (wip)

--- src/Validator/NodeIdempotencyValidator.php
+++ src/Validator/NodeIdempotencyValidator.php
@@ -5,71 +5,70 @@ namespace Gdbots\Ncr\Validator;
 
 use Gdbots\Common\Util\SlugUtils;
 use Gdbots\Ncr\Exception\NodeAlreadyExists;
-use Gdbots\Pbj\Assertion;
-use Gdbots\Pbj\Message;
+use Gdbots\Pbj\SchemaQName;
 use Gdbots\Pbjx\DependencyInjection\PbjxValidator;
 use Gdbots\Pbjx\Event\PbjxEvent;
 use Gdbots\Pbjx\EventSubscriber;
-use Psr\Cache\CacheItemPoolInterface;
+use Gdbots\Schemas\Iam\Mixin\User\User;
+use Gdbots\Schemas\Ncr\Mixin\Node\Node;
 use Psr\Cache\CacheItemInterface;
-use Gdbots\Pbj\SchemaQName;
+use Psr\Cache\CacheItemPoolInterface;
 
 class NodeIdempotencyValidator implements EventSubscriber, PbjxValidator
 {
     /** @var CacheItemPoolInterface */
     protected $cache;
-    protected $ttl;
+
+    /** @var array */
+    protected $ttl = ['default' => 60];
 
     /**
      * @param CacheItemPoolInterface $cache
-     * @param int $ttl sets the expire time for cache items
+     * @param array                  $ttl
      */
-    public function __construct(CacheItemPoolInterface $cache, int $ttl = 60)
+    public function __construct(CacheItemPoolInterface $cache, array $ttl = [])
     {
-      $this->cache = $cache;
-      $this->ttl= $ttl;
+        $this->cache = $cache;
+        // defaults
+        $ttl += ['default' => 60];
+        $this->ttl = $ttl;
     }
 
     /**
      * @param PbjxEvent $pbjxEvent
+     *
+     * @throws NodeAlreadyExists
      */
     public function validateCreateNode(PbjxEvent $pbjxEvent): void
     {
         $command = $pbjxEvent->getMessage();
+        if (!$command->has('node')) {
+            return;
+        }
 
-        Assertion::true($command->has('node'), 'Field "node" is required.', 'node');
+        /** @var Node $node */
+        $node = $command->get('node');
+        if ($this->shouldIgnoreNode($node)) {
+            return;
+        }
+
+        $cacheKeys = $this->getCacheKeys($node);
 
         /** @var CacheItemInterface[] $cacheItems */
-        $cacheItems = [];
-        /** @var Message $node */
-        $node = $command->get('node');
-        $qname = $node::schema()->getQName();
+        $cacheItems = $this->cache->getItems(array_keys($cacheKeys));
 
-        $keys = [
-            $this->getCacheKey($qname, SlugUtils::create($node->get('title'))) => 'title',
-        ];
-        if ($node->has('slug')) {
-            $keys[$this->getCacheKey($qname, $node->get('slug'))] = 'slug';
-        }
-
-        $cacheItems = $this->cache->getItems(array_keys($keys));
-        if ($cacheItems instanceof \Traversable) {
-            $cacheItems = iterator_to_array($cacheItems);
-        }
-
-        /** @var CacheItemInterface $cacheItem */
-        foreach ($keys as $cacheKey => $value) {
-            $cacheItem = $cacheItems[$cacheKey];
+        foreach ($cacheItems as $cacheItem) {
             if (!$cacheItem->isHit()) {
                 continue;
             }
 
+            $field = $cacheKeys[$cacheItem->getKey()];
             throw new NodeAlreadyExists(
                 sprintf(
-                    'The [%s] with [%s] [%s] already exists so [%s] cannot continue.',
+                    'The [%s] with %s [%s] already exists so [%s] cannot continue.',
                     $node::schema()->getCurie()->getMessage(),
-                    $value,
-                    $node->get($value),
+                    $field,
+                    $node->get($field),
                     $command->generateMessageRef()
                 )
             );
@@ -77,60 +76,112 @@ class NodeIdempotencyValidator implements EventSubscriber, PbjxValidator
     }
 
     /**
-     * Handler will actually save the cache items.
+     * Populates cache keys so idempotency check can reject duplicates
+     * after nodes are successfully created.
      *
      * @param PbjxEvent $pbjxEvent
      */
-    public function onCreateNodeAfterHandler(PbjxEvent $pbjxEvent): void
+    public function onCreateNodeAfterHandle(PbjxEvent $pbjxEvent): void
     {
         $command = $pbjxEvent->getMessage();
-        /** @var CacheItemInterface[] $cacheItems */
-        $cacheItems = [];
-        /** @var Message $node */
+        if (!$command->has('node')) {
+            return;
+        }
+
+        /** @var Node $node */
         $node = $command->get('node');
-
-        $qname = $node::schema()->getQName();
-        $keys = [
-            $this->getCacheKey($qname, SlugUtils::create($node->get('title'))) => 'title',
-        ];
-        if ($node->has('slug')) {
-            $keys[$this->getCacheKey($qname, $node->get('slug'))] = 'slug';
+        if ($this->shouldIgnoreNode($node)) {
+            return;
         }
 
-        $cacheItems = $this->cache->getItems(array_keys($keys));
-        if ($cacheItems instanceof \Traversable) {
-            $cacheItems = iterator_to_array($cacheItems);
-        }
+        $cacheKeys = $this->getCacheKeys($node);
 
-        /** @var CacheItemInterface $cacheItem */
+        /** @var CacheItemInterface[] $cacheItems */
+        $cacheItems = $this->cache->getItems(array_keys($cacheKeys));
+
         foreach ($cacheItems as $cacheItem) {
-            $value = $keys[$cacheItem->getKey()];
-            if ($this->ttl > 0) {
-                $cacheItem->expiresAfter($this->ttl);
-            }
-            $this->cache->saveDeferred($cacheItem->set($value));
+            $field = $cacheKeys[$cacheItem->getKey()];
+            $cacheItem->set($field)->expiresAfter($this->getCacheTtl($node));
+            $this->cache->saveDeferred($cacheItem);
         }
     }
 
     /**
-     * Creates the cache key based from the node's qname appended with it's title/slug value.
+     * Override to provide logic which can ignore when the idempotency
+     * check is performed, useful for when you expect to get a lot of
+     * nodes with the same title, email, etc.
      *
-     * @param SchemaQName $qname in format `acme:video`
-     * @param string $sluggifiedValue the sluggified string
-     * @param array $context
+     * @param Node $node
      *
-     * @return string return a string in this format for example `acme_video.some_title.php`
+     * @return bool
      */
-    protected function getCacheKey(SchemaQName $qname, string $sluggifiedValue, array $context = []): string
+    protected function shouldIgnoreNode(Node $node): bool
     {
-        $qnameStr = str_replace(':', '_', $qname);
+        return false;
+    }
+
+    /**
+     * Derive the cache keys to use for the idempotency check
+     * from the node itself.
+     *
+     * @param Node $node
+     *
+     * @return array
+     */
+    protected function getCacheKeys(Node $node): array
+    {
+        $qname = $node::schema()->getQName();
+        $cacheKeys = [];
+
+        if ($node->has('title')) {
+            $cacheKeys[$this->getCacheKey($qname, SlugUtils::create($node->get('title')))] = 'title';
+        }
+
+        if ($node->has('slug')) {
+            $cacheKeys[$this->getCacheKey($qname, $node->get('slug'))] = 'slug';
+        }
+
+        if ($node instanceof User && $node->has('email')) {
+            $cacheKeys[$this->getCacheKey($qname, $node->get('email'))] = 'email';
+        }
+
+        return $cacheKeys;
+    }
+
+    /**
+     * Returns the cache key to use for the provided NodeRef.
+     * This must be compliant with psr6 "Key" definition.
+     *
+     * @link http://www.php-fig.org/psr/psr-6/#definitions
+     *
+     * The ".php" suffix here is used because the cache item
+     * will be stored as serialized php.
+     *
+     * @param SchemaQName $qname
+     * @param string      $key
+     *
+     * @return string
+     */
+    protected function getCacheKey(SchemaQName $qname, string $key): string
+    {
+        // niv (node imdempotency validator) prefix is to avoid collision
         return str_replace('-', '_', sprintf(
-            '%s.%s.php',
-            $qnameStr,
-            $sluggifiedValue
+            'niv.%s.%s.%s.php',
+            $qname->getVendor(),
+            $qname->getMessage(),
+            md5($key)
         ));
     }
 
+    /**
+     * @param Node $node
+     *
+     * @return int
+     */
+    protected function getCacheTtl(Node $node): int
+    {
+        return $this->ttl[$node::schema()->getQName()->toString()] ?? $this->ttl['default'];
+    }
 
     /**
      * @return array
@@ -138,8 +189,8 @@ class NodeIdempotencyValidator implements EventSubscriber, PbjxValidator
     public static function getSubscribedEvents()
     {
         return [
-            'gdbots:ncr:mixin:create-node.validate' => 'validateCreateNode',
-            'gdbots:ncr:mixin:create-node.after_handle' => 'onCreateNodeAfterHandler',
+            'gdbots:ncr:mixin:create-node.validate'     => 'validateCreateNode',
+            'gdbots:ncr:mixin:create-node.after_handle' => 'onCreateNodeAfterHandle',
         ];
     }
 }
