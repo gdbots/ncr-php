4f6409a239868b49c3a4600eee07fdbd7a19a81e
Greg Brown <greg@12dnetworks.com>
2020-06-16 18:03:55 -0700

    v2.x - update NodeIdempotencyValidator

--- src/Exception/NodeAlreadyExists.php
+++ src/Exception/NodeAlreadyExists.php
@@ -3,32 +3,12 @@ declare(strict_types=1);
 
 namespace Gdbots\Ncr\Exception;
 
-use Gdbots\Pbj\Exception\HasEndUserMessage;
 use Gdbots\Schemas\Pbjx\Enum\Code;
 
-final class NodeAlreadyExists extends \RuntimeException implements GdbotsNcrException, HasEndUserMessage
+final class NodeAlreadyExists extends \RuntimeException implements GdbotsNcrException
 {
-    /**
-     * @param string $message
-     */
     public function __construct(string $message = 'Node already exists.')
     {
         parent::__construct($message, Code::ALREADY_EXISTS);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getEndUserMessage()
-    {
-        return $this->getMessage();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getEndUserHelpLink()
-    {
-        return null;
     }
 }
