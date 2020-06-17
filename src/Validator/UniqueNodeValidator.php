35ae333b57a2bdd727478e4f03443476e7593c94
Greg Brown <greg@12dnetworks.com>
2020-06-17 12:50:35 -0700

    v2.x - update UniqueNodeValidator

--- src/Validator/UniqueNodeValidator.php
+++ src/Validator/UniqueNodeValidator.php
@@ -4,99 +4,103 @@ declare(strict_types=1);
 namespace Gdbots\Ncr\Validator;
 
 use Gdbots\Ncr\Exception\NodeAlreadyExists;
-use Gdbots\Ncr\PbjxHelperTrait;
 use Gdbots\Pbj\Assertion;
+use Gdbots\Pbj\Message;
+use Gdbots\Pbj\WellKnown\NodeRef;
 use Gdbots\Pbjx\DependencyInjection\PbjxValidator;
 use Gdbots\Pbjx\Event\PbjxEvent;
 use Gdbots\Pbjx\EventSubscriber;
 use Gdbots\Pbjx\Exception\RequestHandlingFailed;
-use Gdbots\Schemas\Ncr\Mixin\Node\Node;
-use Gdbots\Schemas\Ncr\NodeRef;
+use Gdbots\Schemas\Ncr\Command\CreateNodeV1;
+use Gdbots\Schemas\Ncr\Command\RenameNodeV1;
+use Gdbots\Schemas\Ncr\Command\UpdateNodeV1;
+use Gdbots\Schemas\Ncr\Mixin\CreateNode\CreateNodeV1Mixin;
+use Gdbots\Schemas\Ncr\Mixin\Node\NodeV1Mixin;
+use Gdbots\Schemas\Ncr\Mixin\RenameNode\RenameNodeV1Mixin;
+use Gdbots\Schemas\Ncr\Mixin\Sluggable\SluggableV1Mixin;
+use Gdbots\Schemas\Ncr\Mixin\UpdateNode\UpdateNodeV1Mixin;
+use Gdbots\Schemas\Ncr\Request\GetNodeRequestV1;
+use Gdbots\Schemas\Ncr\Request\GetNodeResponseV1;
 use Gdbots\Schemas\Pbjx\Enum\Code;
 use Gdbots\Schemas\Pbjx\StreamId;
 
 class UniqueNodeValidator implements EventSubscriber, PbjxValidator
 {
-    use PbjxHelperTrait;
+    public static function getSubscribedEvents()
+    {
+        return [
+            CreateNodeV1::SCHEMA_CURIE . '.validate'      => 'validateCreateNode',
+            RenameNodeV1::SCHEMA_CURIE . '.validate'      => 'validateRenameNode',
+            UpdateNodeV1::SCHEMA_CURIE . '.validate'      => 'validateUpdateNode',
+            // deprecated mixins, will be removed in 3.x
+            CreateNodeV1Mixin::SCHEMA_CURIE . '.validate' => 'validateCreateNode',
+            RenameNodeV1Mixin::SCHEMA_CURIE . '.validate' => 'validateRenameNode',
+            UpdateNodeV1Mixin::SCHEMA_CURIE . '.validate' => 'validateUpdateNode',
+        ];
+    }
 
-    /**
-     * @param PbjxEvent $pbjxEvent
-     */
     public function validateCreateNode(PbjxEvent $pbjxEvent): void
     {
         $command = $pbjxEvent->getMessage();
 
-        Assertion::true($command->has('node'), 'Field "node" is required.', 'node');
-        $node = $command->get('node');
+        Assertion::true($command->has(CreateNodeV1::NODE_FIELD), 'Field "node" is required.', 'node');
+        $node = $command->get(CreateNodeV1::NODE_FIELD);
         $nodeRef = NodeRef::fromNode($node);
 
-        if ($node->has('slug')) {
-            $this->ensureSlugIsAvailable($pbjxEvent, $nodeRef, $node->get('slug'));
+        if ($node->has(SluggableV1Mixin::SLUG_FIELD)) {
+            $this->ensureSlugIsAvailable($pbjxEvent, $nodeRef, $node->get(SluggableV1Mixin::SLUG_FIELD));
         }
 
         $this->ensureStreamDoesNotExist($pbjxEvent, $nodeRef);
     }
 
-    /**
-     * @param PbjxEvent $pbjxEvent
-     */
     public function validateRenameNode(PbjxEvent $pbjxEvent): void
     {
         $command = $pbjxEvent->getMessage();
 
-        Assertion::true($command->has('node_ref'), 'Field "node_ref" is required.', 'node_ref');
-        Assertion::true($command->has('new_slug'), 'Field "new_slug" is required.', 'new_slug');
+        Assertion::true($command->has(RenameNodeV1::NODE_REF_FIELD), 'Field "node_ref" is required.', 'node_ref');
+        Assertion::true($command->has(RenameNodeV1::NEW_SLUG_FIELD), 'Field "new_slug" is required.', 'new_slug');
 
         /** @var NodeRef $nodeRef */
-        $nodeRef = $command->get('node_ref');
-        $this->ensureSlugIsAvailable($pbjxEvent, $nodeRef, $command->get('new_slug'));
+        $nodeRef = $command->get(RenameNodeV1::NODE_REF_FIELD);
+        $this->ensureSlugIsAvailable($pbjxEvent, $nodeRef, $command->get(RenameNodeV1::NEW_SLUG_FIELD));
     }
 
-    /**
-     * @param PbjxEvent $pbjxEvent
-     */
     public function validateUpdateNode(PbjxEvent $pbjxEvent): void
     {
         $command = $pbjxEvent->getMessage();
 
-        Assertion::true($command->has('new_node'), 'Field "new_node" is required.', 'new_node');
-        $newNode = $command->get('new_node');
+        Assertion::true($command->has(UpdateNodeV1::NEW_NODE_FIELD), 'Field "new_node" is required.', 'new_node');
+        $newNode = $command->get(UpdateNodeV1::NEW_NODE_FIELD);
 
         /*
          * An update SHOULD NOT change the slug, so copy the slug from
          * the old node if it's present. To change the slug, use proper
          * "rename" command.
          */
-        if ($command->has('old_node')) {
-            $oldNode = $command->get('old_node');
+        if ($command->has(UpdateNodeV1::OLD_NODE_FIELD)) {
+            $oldNode = $command->get(UpdateNodeV1::OLD_NODE_FIELD);
 
-            if ($oldNode->has('slug')) {
-                $newNode->set('slug', $oldNode->get('slug'));
+            if ($oldNode->has(SluggableV1Mixin::SLUG_FIELD)) {
+                $newNode->set(SluggableV1Mixin::SLUG_FIELD, $oldNode->get(SluggableV1Mixin::SLUG_FIELD));
             }
         }
     }
 
-    /**
-     * @param PbjxEvent $pbjxEvent
-     * @param NodeRef   $nodeRef
-     * @param string    $slug
-     *
-     * @throws NodeAlreadyExists
-     */
     protected function ensureSlugIsAvailable(PbjxEvent $pbjxEvent, NodeRef $nodeRef, string $slug): void
     {
         $pbjx = $pbjxEvent::getPbjx();
         $command = $pbjxEvent->getMessage();
 
         try {
-            $request = $this->createGetNodeRequest($command, $nodeRef, $pbjx)
-                ->set('consistent_read', true)
-                ->set('qname', $nodeRef->getQName()->toString())
-                ->set('slug', $slug);
+            $request = GetNodeRequestV1::create()
+                ->set(GetNodeRequestV1::CONSISTENT_READ_FIELD, true)
+                ->set(GetNodeRequestV1::QNAME_FIELD, $nodeRef->getQName()->toString())
+                ->set(GetNodeRequestV1::SLUG_FIELD, $slug);
 
             $response = $pbjx->copyContext($command, $request)->request($request);
         } catch (RequestHandlingFailed $e) {
-            if (Code::NOT_FOUND === $e->getResponse()->get('error_code')) {
+            if (Code::NOT_FOUND === $e->getCode()) {
                 // this is what we want
                 return;
             }
@@ -106,10 +110,10 @@ class UniqueNodeValidator implements EventSubscriber, PbjxValidator
             throw $t;
         }
 
-        /** @var Node $node */
-        $node = $response->get('node');
+        /** @var Message $node */
+        $node = $response->get(GetNodeResponseV1::NODE_FIELD);
 
-        if ($nodeRef->getId() === (string)$node->get('_id')) {
+        if ($nodeRef->getId() === $node->fget(NodeV1Mixin::_ID_FIELD)) {
             // this is the same node.
             return;
         }
@@ -124,25 +128,18 @@ class UniqueNodeValidator implements EventSubscriber, PbjxValidator
         );
     }
 
-    /**
-     * @param PbjxEvent $pbjxEvent
-     * @param NodeRef   $nodeRef
-     *
-     * @throws NodeAlreadyExists
-     */
     protected function ensureStreamDoesNotExist(PbjxEvent $pbjxEvent, NodeRef $nodeRef): void
     {
         $pbjx = $pbjxEvent::getPbjx();
         $message = $pbjxEvent->getMessage();
 
-        $streamId = $this->createStreamId($nodeRef);
         $slice = $pbjx->getEventStore()->getStreamSlice(
-            $streamId,
+            StreamId::fromNodeRef($nodeRef),
             null,
             1,
             true,
             true,
-            $this->createEventStoreContext($message, $streamId)
+            ['causator' => $message]
         );
 
         if ($slice->count()) {
@@ -155,27 +152,5 @@ class UniqueNodeValidator implements EventSubscriber, PbjxValidator
                 )
             );
         }
-    }
-
-    /**
-     * @param NodeRef $nodeRef
-     *
-     * @return StreamId
-     */
-    protected function createStreamId(NodeRef $nodeRef): StreamId
-    {
-        return StreamId::fromString(sprintf('%s.history:%s', $nodeRef->getLabel(), $nodeRef->getId()));
-    }
-
-    /**
-     * @return array
-     */
-    public static function getSubscribedEvents()
-    {
-        return [
-            'gdbots:ncr:mixin:create-node.validate' => 'validateCreateNode',
-            'gdbots:ncr:mixin:rename-node.validate' => 'validateRenameNode',
-            'gdbots:ncr:mixin:update-node.validate' => 'validateUpdateNode',
-        ];
     }
 }
