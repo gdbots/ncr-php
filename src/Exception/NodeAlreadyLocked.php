e79db65a1d45d96bcd2a23b12aa5fbf0197270e0
Greg Brown <greg@12dnetworks.com>
2020-06-17 13:47:02 -0700

    v2.x - update exceptions

--- src/Exception/NodeAlreadyLocked.php
+++ src/Exception/NodeAlreadyLocked.php
@@ -3,32 +3,12 @@ declare(strict_types=1);
 
 namespace Gdbots\Ncr\Exception;
 
-use Gdbots\Pbj\Exception\HasEndUserMessage;
 use Gdbots\Schemas\Pbjx\Enum\Code;
 
-final class NodeAlreadyLocked extends \RuntimeException implements GdbotsNcrException, HasEndUserMessage
+final class NodeAlreadyLocked extends \RuntimeException implements GdbotsNcrException
 {
-    /**
-     * @param string $message
-     */
     public function __construct(string $message = 'Node already locked.')
     {
         parent::__construct($message, Code::FAILED_PRECONDITION);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getEndUserMessage()
-    {
-        return $this->getMessage();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getEndUserHelpLink()
-    {
-        return null;
     }
 }
